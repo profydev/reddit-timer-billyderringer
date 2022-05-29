48bda22bb2dc8e87a0498f9f7c58e14a2cf8ef5a
Billy Derringer <billyderringer7@gmail.com>
2022-05-29 13:56:07 -0500

    Add header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from './components/header/Header';
 import './normalize.css';
 import './index.css';
 
 function App() {
   return (
     <div>
+      <Header />
       Home Page
     </div>
   );
